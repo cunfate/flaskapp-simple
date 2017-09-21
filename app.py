49ccada2b79f88c2e3f1b8400bedfa3f865a4204
cunFate <cun.zhang@outlook.com>
2017-09-22 00:38:10 +0800

    add form

--- app.py
+++ app.py
@@ -11,23 +11,30 @@ app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY_FLASK')
+
 
 @app.route('/')
 def index():
-    return '<h1>Hello bitches</h1>'
+    return render_template('index.html')
+
 
 @app.route('/hello')
 def helloPage():
     user_agent = request.headers.get('User-Agent')
     return '<p>Your browser is %s</p>' % user_agent
 
+
 @app.route('/base')
 def basePage():
-    return render_template('base.html', title='Hello', body='<p>I love you1</p>')
+    return render_template('base.html', title='Hello',
+                           body='<p>I love you1</p>')
+
 
 @app.route('/hello/<name>')
 def helloname(name):
-    return render_template('user.html', name=name)
+    return render_template('hello.html', name=name)
+
 
 if __name__ == '__main__':
     manager.run()
