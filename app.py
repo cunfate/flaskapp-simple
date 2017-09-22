923e28a54c9fdd59e8fbdaa4b9b34c295d0e2bcf
zhangcun <cun.zhang@outlook.com>
2017-09-22 04:27:40 -0700

    add form with bootstrap

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask import Flask, render_template
 from flask import request
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm as Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -14,9 +17,19 @@ bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY_FLASK')
 
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index-test.html', name=name, form=form)
 
 
 @app.route('/hello')
