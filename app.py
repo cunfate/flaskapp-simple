1eacab1d51b8432205bbb33d9e744315c9951b14
cunFate <cun.zhang@outlook.com>
2017-09-26 23:19:46 +0800

    add mysql support,use sqlalchemy

--- app.py
+++ app.py
@@ -5,16 +5,44 @@ from flask import request
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm as Form
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+mysql_username = os.getenv('MYSQL_USERNAME')
+mysql_pwd = os.getenv('MYSQL_PWD_FLASK')
+print(mysql_username)
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY_FLASK')
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'mysql+pymysql://%s:%s@127.0.0.1/flaskapp?charset=utf8'\
+    % (mysql_username, mysql_pwd)
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class NameForm(Form):
