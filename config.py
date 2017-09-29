953d05e513a642872641560e296f8ca39a719a65
zhangcun <cun.zhang@outlook.com>
2017-09-29 05:04:46 -0700

    add some config file

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY_FLASK')
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
+    @staticmethod
+    def init_app(app):
+        pass
