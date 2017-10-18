f4247df6892ff0155daf25a0f43034b5fc39d799
cunFate <cun.zhang@outlook.com>
2017-10-18 22:27:06 +0800

    add profile editor

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from flask import render_template, session, redirect, url_for, flash, abort
+from flask_login import current_user, login_required
 
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
 from .. import db
 from ..models import User
 from ..decorators import admin_required
@@ -43,3 +44,21 @@ def user_page(username):
     if user is None:
         abort(404)
     return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        db.session.commit()
+        flash('Your profile has been upadated.')
+        return redirect(url_for('.user_page', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
