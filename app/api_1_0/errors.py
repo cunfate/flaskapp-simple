e9f61093415074c84b5b524b76d9423d62f32bd5
zhangcun <cun.zhang@outlook.com>
2017-10-26 03:37:54 -0700

    temp

---
+++ app/api_1_0/errors.py
@@ -0,0 +1,23 @@
+from . import api
+from flask import jsonify
+
+
+@api.errorhandler(403)
+def forbidden(message):
+    response = jsonify({'error': 'forbidden', 'message': message})
+    response.status_code = 403
+    return response
+
+
+@api.errorhandler(404)
+def notfound(message):
+    response = jsonify({'error': 'not found', 'message': message})
+    response.status_code = 404
+    return response
+
+
+@api.errorhandler(500)
+def internaleror(message):
+    response = jsonify({'error': 'internal server error', 'message': message})
+    response.status_code = 500
+    return response
