7e85d070c82bae73a266f604912867ca5f4d717d
zhangcun <cun.zhang@outlook.com>
2017-11-08 04:31:12 -0800

    add avatar croper js file

---
+++ static/js/cropavtar.js
@@ -0,0 +1,42 @@
+jQuery(function($){
+    // The variable jcrop_api will hold a reference to the
+    // Jcrop API once Jcrop is instantiated.
+    var jcrop_api;
+
+    // In this example, since Jcrop may be attached or detached
+    // at the whim of the user, I've wrapped the call into a function
+    initJcrop();
+    
+    // The function is pretty simple
+    function initJcrop()//
+    {
+      // Hide any interface elements that require Jcrop
+      // (This is for the local user interface portion.)
+      $('.requiresjcrop').hide();
+
+      // Invoke Jcrop in typical fashion
+      $('#avatar_upload').Jcrop({
+        onRelease: releaseCheck
+      },function(){
+
+        jcrop_api = this;
+        jcrop_api.animateTo([100,100,400,300]);
+		//Set the resize function disable.
+      	jcrop_api.setOptions({ allowResize: false });
+      	jcrop_api.focus();
+
+        // Setup and dipslay the interface for "enabled"
+        // Setup and dipslay the interface for "enabled"
+        $('#can_click,#can_move,#can_size').attr('checked','checked');
+        $('#ar_lock,#size_lock,#bg_swap').attr('checked',false);
+        $('.requiresjcrop').show();
+
+      });
+
+    }
+
+	function getSelect()
+	{
+		return jcrop_api.tellSelect();
+	}
+}
